c3a6f5dd667d3edef7ec9ab384898cc5c7d31c1c
Christopher Poenaru <kiambogo@gmail.com>
2017-03-16 01:53:23 -0400

    Segment Ratings
      - Added ratings to segments
      - Removed start/end points from segment form; using head/tail on points instead

--- app/db/Tables.scala
+++ app/db/Tables.scala
@@ -14,11 +14,11 @@ object Tables {
   private[this] def point2Pts(point: Point): JTSPoint = geometryFactory.createPoint(new Coordinate(point.lng, point.lat))
 
 
-   private[this] def segmentTupled: ((UUID, Option[String], Option[String], JTSPoint, JTSPoint, Option[String])) => Segment = {
-      case (id: UUID, name: Option[String], description: Option[String], start: JTSPoint, end: JTSPoint, polyline: Option[String]) =>
-        Segment(id, name, description, pts2Point(start), pts2Point(end), polyline)
+   private[this] def segmentTupled: ((UUID, Option[String], Option[String], JTSPoint, JTSPoint, String, Double)) => Segment = {
+      case (id: UUID, name: Option[String], description: Option[String], start: JTSPoint, end: JTSPoint, polyline: String, rating: Double) =>
+        Segment(id, name, description, pts2Point(start), pts2Point(end), polyline, rating)
     }
-   private[this] def segmentUnapply(seg: Segment) = Some(seg.id, seg.name, seg.description, point2Pts(seg.start), point2Pts(seg.end), seg.polyline)
+   private[this] def segmentUnapply(seg: Segment) = Some(seg.id, seg.name, seg.description, point2Pts(seg.start), point2Pts(seg.end), seg.polyline, seg.rating)
 
   class Segments(tag: Tag) extends Table[Segment](tag, "segments") {
     def id = column[UUID]("id", O.PrimaryKey)
@@ -26,9 +26,10 @@ object Tables {
     def description = column[Option[String]]("description")
     def startPoint = column[JTSPoint]("start_point")
     def endPoint = column[JTSPoint]("end_point")
-    def polyline = column[Option[String]]("polyline")
+    def polyline = column[String]("polyline")
+    def rating = column[Double]("rating")
 
-    override def * = (id, name, description, startPoint, endPoint, polyline) <> (segmentTupled, segmentUnapply)
+    override def * = (id, name, description, startPoint, endPoint, polyline, rating) <> (segmentTupled, segmentUnapply)
   }
 
   val segments = TableQuery[Segments]
