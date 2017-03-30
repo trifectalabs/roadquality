8c55d0059642c0afb20d67bd730de81f7b1ee40b
Christopher Poenaru <kiambogo@gmail.com>
2017-03-30 01:08:31 -0400

    Added logback config; log each request

--- app/controllers/Routes.scala
+++ app/controllers/Routes.scala
@@ -7,23 +7,24 @@ import com.trifectalabs.road.quality.v0.models.json._
 import services.RoutingService
 import play.api.libs.json.Json
 import play.api.mvc.{Action, Controller}
+import util.actions.LoggingAction
 
 import scala.concurrent.ExecutionContext
 
 class Routes @Inject() (routingService: RoutingService)(implicit ec: ExecutionContext) extends Controller {
 
-  def get(start_lat: Double, start_lng: Double, end_lat: Double, end_lng: Double) = Action.async {
+  def get(start_lat: Double, start_lng: Double, end_lat: Double, end_lng: Double) = LoggingAction.async  {
     routingService.generateRoute(start_lat, start_lng, end_lat, end_lng).map { r =>
         println(s"Polyline: $r")
         Ok(Json.toJson(r))
     }
   }
 
-  def getSnap(lat: Double, lng: Double) = Action.async {
+  def getSnap(lat: Double, lng: Double) = LoggingAction.async {
     routingService.snapPoint(Point(lat, lng)).map(p => Ok(Json.toJson(p)))
   }
 
-  def post() = Action.async(parse.json[Seq[Point]]) { implicit request =>
+  def post() = LoggingAction.async(parse.json[Seq[Point]]) { implicit request =>
     val points = request.body
     routingService.generateRoute(points).map { r =>
       Ok(Json.toJson(r))
