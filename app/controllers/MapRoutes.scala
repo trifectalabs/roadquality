6a7d3c2219cd9033c43ee87e93db5f2c24988da3
Christopher Poenaru <kiambogo@gmail.com>
2017-04-02 01:23:10 -0400

    Added AuthLogginAction for JWT

--- app/controllers/MapRoutes.scala
+++ app/controllers/MapRoutes.scala
@@ -7,24 +7,25 @@ import com.trifectalabs.road.quality.v0.models.json._
 import services.RoutingService
 import play.api.libs.json.Json
 import play.api.mvc.{Action, Controller}
-import util.actions.LoggingAction
+import util.actions.AuthLoggingAction
 
 import scala.concurrent.ExecutionContext
 
-class MapRoutes @Inject() (routingService: RoutingService)(implicit ec: ExecutionContext) extends Controller {
+class MapRoutes @Inject() (routingService: RoutingService, authLoggingAction: AuthLoggingAction)(implicit ec: ExecutionContext) extends Controller {
+  import authLoggingAction._
 
-  def get(start_lat: Double, start_lng: Double, end_lat: Double, end_lng: Double) = LoggingAction.async  {
+  def get(start_lat: Double, start_lng: Double, end_lat: Double, end_lng: Double) = AuthLoggingAction.async  {
     routingService.generateRoute(start_lat, start_lng, end_lat, end_lng).map { r =>
         println(s"Polyline: $r")
         Ok(Json.toJson(r))
     }
   }
 
-  def getSnap(lat: Double, lng: Double) = LoggingAction.async {
+  def getSnap(lat: Double, lng: Double) = AuthLoggingAction.async {
     routingService.snapPoint(Point(lat, lng)).map(p => Ok(Json.toJson(p)))
   }
 
-  def post() = LoggingAction.async(parse.json[Seq[Point]]) { implicit request =>
+  def post() = AuthLoggingAction.async(parse.json[Seq[Point]]) { implicit request =>
     val points = request.body
     routingService.generateRoute(points).map { r =>
       Ok(Json.toJson(r))
--- app/util/JwtUtil.scala
+++ app/util/JwtUtil.scala
@@ -4,13 +4,26 @@ import javax.inject.Inject
 
 import play.api.Configuration
 import play.api.libs.json.Json._
+import scala.util._
 
 import pdi.jwt.{Jwt, JwtAlgorithm, JwtHeader, JwtClaim, JwtOptions}
 import com.trifectalabs.road.quality.v0.models.{ User, UserRole }
 import com.trifectalabs.road.quality.v0.models.json._
 
-class JWT @Inject() (configuration: Configuration) {
+class JwtUtil @Inject() (configuration: Configuration) {
   lazy val secret = configuration.getString("play.crypto.secret").get
+
+  def isTokenValid(token: String): Boolean = {
+    Jwt.isValid(token, secret, Seq(JwtAlgorithm.HS256))
+    // TODO check if token exists
+  }
+
+  def decodeToken(token: String): Option[User] = {
+    Jwt.decode(token, secret, Seq(JwtAlgorithm.HS256)) match {
+      case Success(t) => parse(t).asOpt[User]
+      case Failure(e) => None
+    }
+  }
 
 	def createToken(user: User): String = {
     Jwt.encode(stringify(toJson(user)), secret, JwtAlgorithm.HS256)
