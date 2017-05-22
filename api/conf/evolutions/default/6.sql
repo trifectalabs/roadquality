5dbf7ca03c887ef208d3fc4e846cde9974e176b7
Christopher Poenaru <kiambogo@gmail.com>
2017-05-22 17:08:34 -0400

    Migrated routing function

--- api/conf/evolutions/default/6.sql
+++ api/conf/evolutions/default/6.sql
@@ -1,34 +1,71 @@
-# Postgis/Pgrouting functions
+# Add rating colour functions
 
 # --- !Ups
 
-CREATE OR REPLACE FUNCTION shortest_distance_route(start_lon double precision, start_lat double precision, end_lon double precision, end_lat double precision)
-RETURNS TABLE (x double precision, y double precision) AS $$
+CREATE TABLE colours (
+    level integer,
+    red integer,
+    green integer,
+    blue integer,
+    CONSTRAINT level_key PRIMARY KEY(level)
+);
+
+INSERT INTO colours VALUES (0, 110, 32, 32);
+INSERT INTO colours VALUES (1, 227, 59, 59);
+INSERT INTO colours VALUES (2, 255, 166, 0);
+INSERT INTO colours VALUES (3, 255, 250, 94);
+INSERT INTO colours VALUES (4, 48, 219, 60);
+INSERT INTO colours VALUES (5, 22, 105, 28);
+
+CREATE OR REPLACE FUNCTION blend(start_val integer, end_val integer, ratio double precision)
+RETURNS integer AS $$
+BEGIN
+	RETURN ROUND(start_val + (end_val - start_val) * ratio);;
+END;;
+$$ LANGUAGE plpgsql;
+
+
+CREATE TYPE rgb as (r int, g int, b int);
+CREATE OR REPLACE FUNCTION ratingColour(rating double precision)
+RETURNS TABLE (t text) AS $$
 BEGIN
   RETURN QUERY
-  WITH start_road      AS (SELECT * from closest_road_to_point(start_lon, start_lat)),
-       end_road        AS (SELECT * from closest_road_to_point(end_lon, end_lat)),
-       start_point     AS (SELECT ST_GeometryFromText('POINT('||start_lon||' '||start_lat||')',4326)),
-       end_point       AS (SELECT ST_GeometryFromText('POINT('||end_lon||' '||end_lat||')',4326)),
-       route           AS (SELECT * from pgr_trsp('SELECT id::integer, source::integer, target::integer, distance::float8 as cost FROM planet_osm_line_noded',
-                          (SELECT id from start_road)::integer,
-                          (SELECT ST_LineLocatePoint((SELECT road from start_road), (SELECT * FROM start_point))),
-                          (SELECT id from end_road)::integer,
-                          (SELECT ST_LineLocatePoint((SELECT road from end_road), (SELECT * FROM end_point))),
-                          false, false) AS r INNER JOIN planet_osm_line_noded as ways on ways.id = r.id2
-                          where r.seq <> 1 and r.id2 <> ((SELECT id from end_road)::integer)),
-       corrected_start AS (SELECT ST_SetPoint((SELECT ST_MakeLine(result.way) FROM route AS result), 0,
-                          (ST_LineInterpolatePoint(
-                          (SELECT way from planet_osm_line_noded where id = (SELECT id from start_road)),
-                          (SELECT ST_LineLocatePoint((SELECT road from start_road), (SELECT * FROM start_point))))))),
-       corrected_path  AS (SELECT ST_SetPoint((SELECT * FROM corrected_start), -1, (ST_LineInterpolatePoint(
-                          (SELECT way from planet_osm_line_noded where id = (SELECT id from end_road)),
-                          (SELECT ST_LineLocatePoint((SELECT road from end_road), (SELECT * FROM end_point))))))),
-       result          AS (SELECT ST_X((ST_dumppoints((SELECT * FROM corrected_path))).geom), ST_Y((ST_dumppoints((SELECT * FROM corrected_path))).geom))
-  SELECT * from result;;
+    WITH
+        startVal AS
+            (SELECT CASE
+                 WHEN rating < 1.8 THEN (SELECT (c.red, c.green, c.blue)::rgb FROM colours c WHERE level = 0)
+                 WHEN rating < 2.6 THEN (SELECT (c.red, c.green, c.blue)::rgb FROM colours c WHERE level = 1)
+                 WHEN rating < 3.4 THEN (SELECT (c.red, c.green, c.blue)::rgb FROM colours c WHERE level = 2)
+                 WHEN rating < 4.2 THEN (SELECT (c.red, c.green, c.blue)::rgb FROM colours c WHERE level = 3)
+                 ELSE (SELECT (c.red, c.green, c.blue)::rgb FROM colours c WHERE level = 4)
+            END),
+        endVal AS
+            (SELECT CASE
+                 WHEN rating < 1.8 THEN (SELECT (c.red, c.green, c.blue)::rgb FROM colours c WHERE level = 1)
+                 WHEN rating < 2.6 THEN (SELECT (c.red, c.green, c.blue)::rgb FROM colours c WHERE level = 2)
+                 WHEN rating < 3.4 THEN (SELECT (c.red, c.green, c.blue)::rgb FROM colours c WHERE level = 3)
+                 WHEN rating < 4.2 THEN (SELECT (c.red, c.green, c.blue)::rgb FROM colours c WHERE level = 4)
+                 ELSE (SELECT (c.red, c.green, c.blue)::rgb FROM colours c WHERE level = 5)
+            END),
+        normRating AS
+            (SELECT CASE
+                 WHEN rating < 1.8 THEN (rating - 1) / 0.8
+                 WHEN rating < 2.6 THEN (rating - 1.8) / 0.8
+                 WHEN rating < 3.4 THEN (rating - 2.6) / 0.8
+                 WHEN rating < 4.2 THEN (rating - 3.4) / 0.8
+                 ELSE (rating - 4.2) / 0.8
+            END),
+        red AS (SELECT blend((SELECT * FROM startVal).r, (SELECT * from endVal).r, (SELECT * FROM normRating))),
+        green AS (SELECT blend((SELECT * from startVal).g, (SELECT * from endVal).g, (SELECT * FROM normRating))),
+        blue AS (SELECT blend((SELECT * from startVal).b, (SELECT * from endVal).b, (SELECT * FROM normRating)))
+        Select '#' || lpad(to_hex(re.blend), 2, '0') || lpad(to_hex(gr.blend), 2, '0') || lpad(to_hex(bl.blend), 2, '0')
+        FROM (SELECT * FROM red) as re, (SELECT * FROM green) as gr, (SELECT * FROM blue) as bl;;
 END;;
 $$ LANGUAGE plpgsql;
 
 	# --- !Downs
 
-DROP FUNCTION shortest_distance_route(double precision, double precision, double precision, double precision);
+DROP TABLE colours;
+DROP FUNCTION blend(integer, integer, double precision);
+DROP TYPE rgb;
+DROP FUNCTION ratingColour(double precision);
