7613ccee80a61c69d0ba62f39bc588cf457838f3
Christopher Poenaru <kiambogo@gmail.com>
2017-05-31 10:34:39 -0400

    Updating frontend to use prod tileserver

--- web/public/javascripts/init.js
+++ web/public/javascripts/init.js
@@ -18,7 +18,7 @@ app.ports.up.subscribe(function() {
       zoomControl: false
   });
 
-  L.tileLayer("http://localhost:8080/roadquality/{z}/{x}/{y}.png", {
+  L.tileLayer("http://tiles.roadquality.org/roadquality/{z}/{x}/{y}.png", {
     attribution: "&copy; <a href='http://osm.org/copyright'>OpenStreetMap</a> contributors"
   }).addTo(myMap);
 
