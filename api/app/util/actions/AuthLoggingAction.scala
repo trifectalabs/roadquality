7c8178bb47626943c67c844a2f088dee02f712f7
Christopher Poenaru <kiambogo@gmail.com>
2017-05-31 22:14:34 -0400

    JWT token validation endpoint

--- api/app/util/actions/AuthLoggingAction.scala
+++ api/app/util/actions/AuthLoggingAction.scala
@@ -4,23 +4,35 @@ import javax.inject.Inject
 
 import org.slf4j.LoggerFactory
 import play.api.mvc._
-import scala.concurrent.Future
+import play.api.mvc.Results._
+import scala.concurrent.{ ExecutionContext, Future }
 import util.JwtUtil
 
 import com.trifectalabs.roadquality.v0.models.User
 
-class AuthLoggingAction @Inject() (jwt: JwtUtil) {
+class AuthLoggingAction @Inject() (jwt: JwtUtil)(implicit ec: ExecutionContext) {
   object AuthLoggingAction extends ActionBuilder[Request] {
     val logger = LoggerFactory.getLogger("requests")
 
     def invokeBlock[A](request: Request[A], block: (Request[A]) => Future[Result]) = {
-      val user = request.headers.get("Authorization").flatMap { authHeader =>
-        val token = authHeader.replace("Bearer", "").trim
-        jwt.decodeToken(token)
-      }
+      val tokenOpt = request.headers.get("Authorization").map { _.replace("Bearer", "").trim }
+      val userOpt = tokenOpt.flatMap(jwt.decodeToken(_))
 
-      logger.info(s"User ID: ${user.map(f => f.id).getOrElse("")}, URI: ${request.path}, Method: ${request.method}, Params: ${request.queryString}, Body: ${request.body.toString}")
-      block(request)
+      userOpt match {
+        case Some(user) =>
+          logger.info(s"User ID: ${user.id}, URI: ${request.path}, Method: ${request.method}, Params: ${request.queryString}, Body: ${request.body.toString}")
+          block(request)
+        case None =>
+          val message = tokenOpt match {
+            case Some(token) =>
+              logger.info(s"Invalid/expired JWT token received: ${token}")
+              "Invalid/expired JWT token"
+            case None =>
+              logger.info(s"No JWT token present in request")
+              "No JWT present in request"
+          }
+          Future(Unauthorized(message))
+      }
     }
   }
 }
--- web/app/controllers/Login.scala
+++ web/app/controllers/Login.scala
@@ -5,13 +5,22 @@ import java.util.UUID
 
 import play.api.mvc.{Action, Controller}
 import play.api.libs.json.Json
+import scala.concurrent.{ ExecutionContext, Future }
 
+import util.actions.AuthLoggingAction
 import util.OAuth2
+import util.JwtUtil
 
-class Login @Inject() (oauth2: OAuth2) extends Controller {
+class Login @Inject() (oauth2: OAuth2, authLoggingAction: AuthLoggingAction)(implicit ec: ExecutionContext) extends Controller {
+  import authLoggingAction._
+
   def oauth() = Action {
     val randomState = UUID.randomUUID().toString
     val redirectUrl = oauth2.getAuthorizationUrl(randomState)
     Redirect(redirectUrl).withSession("oauth-state" -> randomState)
+  }
+
+  def isTokenValid() = AuthLoggingAction.async {
+    Future(Ok("true"))
   }
 }
