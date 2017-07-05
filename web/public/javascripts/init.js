7bf51d1ce69fa4367834bcb4db09c809fbd14810
Josiah Witt <josiah@witt.life>
2017-07-04 20:28:35 -0400

    Redesign map ports

--- web/public/javascripts/init.js
+++ web/public/javascripts/init.js
@@ -6,23 +6,12 @@ let viewOnly = true;
 let popup;
 let showingLayer;
 let cursorClass;
+let tileServerUrl;
 
-let emptySource = {
-    "type": "FeatureCollection",
-    "features": []
-};
-
-let markerSource = function(coords) {
-    return {
-        "type": "FeatureCollection",
-        "features": [{
-            "type": "Feature",
-            "geometry": {
-                "type": "Point",
-                "coordinates": coords
-            }
-        }]
-    };
+if (location.host === "localhost:9000") {
+    tileServerUrl = "http://localhost:8080"
+} else {
+    tileServerUrl = "https://tiles.roadquality.org"
 }
 
 let surfaceLayer = {
@@ -30,7 +19,7 @@ let surfaceLayer = {
     "type": "line",
     "source": {
         type: "vector",
-        tiles: ["https://tiles.roadquality.org/surface_quality/{z}/{x}/{y}.pbf"]
+        tiles: [tileServerUrl + "/surface_quality/{z}/{x}/{y}.pbf"]
     },
     "source-layer": "surface_mini_segments",
     "paint": {
@@ -47,7 +36,7 @@ let trafficLayer = {
     "type": "line",
     "source": {
         type: "vector",
-        tiles: ["https://tiles.roadquality.org/traffic/{z}/{x}/{y}.pbf"]
+        tiles: [tileServerUrl + "/traffic/{z}/{x}/{y}.pbf"]
     },
     "source-layer": "traffic_mini_segments",
     "paint": {
@@ -59,19 +48,18 @@ let trafficLayer = {
     }
 };
 
-// STORE SESSION
+// ELM PORTS
+
 app.ports.storeSession.subscribe(function(session) {
     localStorage.session = session;
 });
 
-// SESSION CHANGE
 window.addEventListener("storage", function(event) {
     if (event.storageArea === localStorage && event.key === "session") {
         app.ports.onSessionChange.send(event.newValue);
     }
 }, false);
 
-// SETUP MAP
 app.ports.up.subscribe(function(location) {
     // TODO: there must be a better way :(
     setTimeout(function() {
@@ -85,7 +73,9 @@ app.ports.up.subscribe(function(location) {
         let city = location[0];
         let province = location[1];
         let country = location[2];
-        let url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + city + ",+" + province + ",+" + country + "&key=AIzaSyAzfXJoOAt7m6C4ocTX_-odOj739BqtXts";
+        let url = "https://maps.googleapis.com/maps/api/geocode/json?address=" +
+            city + ",+" + province + ",+" + country +
+            "&key=AIzaSyAzfXJoOAt7m6C4ocTX_-odOj739BqtXts";
         fetch(url).then(function(response) {
             return response.json();
         }).then(function(data) {
@@ -95,12 +85,206 @@ app.ports.up.subscribe(function(location) {
     }, 100);
 });
 
+app.ports.down.subscribe(function() {
+    if (map) {
+        map.remove();
+        map = null;
+    }
+});
+
+app.ports.setLayer.subscribe(function(layer) {
+    if (showingLayer === layer) {
+        return;
+    } else if (map.getLayer(layer)) {
+        map.setLayoutProperty(showingLayer, "visibility", "none");
+        map.setLayoutProperty(layer, "visibility", "visible");
+    }
+    showingLayer = layer;
+});
+
+app.ports.refreshLayer.subscribe(function(layer) {
+    map.removeLayer(layer);
+    map.removeSource(layer);
+    if (layer === "SurfaceQuality") {
+        let dirtySurfaceLayer = Object.assign({}, surfaceLayer);
+        dirtySurfaceLayer.source.tiles[0] += "?dirty=" + Math.random();
+        map.addLayer(dirtySurfaceLayer);
+    } else {
+        let dirtyTrafficLayer = Object.assign({}, trafficLayer);
+        dirtyTrafficLayer.source.tiles[0] += "?dirty=" + Math.random();
+        map.addLayer(dirtyTrafficLayer);
+    }
+});
+
+app.ports.isRouting.subscribe(function(isRouting) {
+    viewOnly = !isRouting;
+    if (isRouting) {
+        map.on("click", click);
+        map.on("contextmenu", contextMenu);
+    } else {
+        map.off("click", click);
+        map.off("contextmenu", contextMenu);
+    }
+});
+
+app.ports.hideSources.subscribe(function(keys) {
+    let emptySource = {
+        "type": "FeatureCollection",
+        "features": []
+    };
+    for (let i = 0; i < keys.length; i++) {
+        map.getSource(keys[i]).setData(emptySource);
+    }
+});
+
+app.ports.addSource.subscribe(function(values) {
+    let key = values[0];
+    let layerType = values[1];
+    let paint = values[2];
+    let coords = [];
+    let geomType;
+    if (values[3].length === 1) {
+        coords = [values[3][0][0], values[3][0][1]];
+        geomType = "Point";
+    } else {
+        for (let i = 0; i < values[3].length; i++) {
+            coords.push([values[3][i][1], values[3][i][0]]);
+        }
+        geomType = "LineString";
+    }
+    let source = makeSource(geomType, coords);
+    if (map.getSource(key)) {
+        map.getSource(key).setData(source);
+    } else {
+        map.addSource(key, {
+            "type": "geojson",
+            "data": source
+        });
+        map.addLayer({
+            "id": key,
+            "type": layerType,
+            "source": key,
+            "paint": paint
+        });
+        if (geomType === "Point") {
+            map.on("mouseenter", key, mouseEnter(key));
+            map.on("mouseleave", key, mouseLeave);
+        }
+    }
+    if (geomType === "LineString") map.moveLayer(key, "startMarker");
+});
+
+// EVENT HANDLERS
+
+function click(e) {
+    if (cursorOverPoint !== null || isDragging) return;
+
+    app.ports.setAnchor.send([e.lngLat.lng, e.lngLat.lat]);
+
+    // Shift LngLat for map pan to compensate for open sidebar menu
+    let mapBounds = map.getBounds();
+    let mapMinX = mapBounds.getWest();
+    let mapMaxX = mapBounds.getEast();
+    let windowMinX = 0;
+    let windowMaxX = window.innerWidth;
+    let mapPosX = e.lngLat.lng;
+    let mapPosY = e.lngLat.lat;
+    let windowPosX = windowMaxX / (mapMaxX - mapMinX) * (mapPosX - mapMinX);
+    let shiftedWindowPosX = windowPosX - 200;
+    let shiftedMapPosX =
+        mapMinX + (mapMaxX - mapMinX) / windowMaxX * shiftedWindowPosX;
+    let shiftedLngLat = [shiftedMapPosX, mapPosY];
+
+    // Calculate pan duration based on distance to pan
+    let maxDistX = (window.innerWidth - 400) / 2;
+    let posX = Math.abs(e.originalEvent.x - 400 - maxDistX);
+    let panX = 0.25 + (0.6 / maxDistX) * (posX - 400);
+    let maxDistY = window.innerHeight / 2;
+    let posY = Math.abs(e.originalEvent.y - maxDistY);
+    let panY = 0.25 + (0.6 / maxDistY) * posY;
+    let panDuration = Math.max(panX, panY) * 1000;
+
+    map.panTo(shiftedLngLat, { duration: panDuration });
+}
+
+function contextMenu(e) {
+    if (cursorOverPoint !== null && cursorOverPoint !== "startMarker") {
+        if (popup) {
+            popup.remove();
+        }
+        popup = new mapboxgl.Popup()
+            .setLngLat(e.lngLat)
+            .setHTML("<span style='cursor: pointer' onClick='removeMarker(\"" + 
+                cursorOverPoint + "\")'>Delete Point</span>")
+            .addTo(map);
+    }
+}
+
+function mouseUp(e) {
+    if (!isDragging) {
+        removeClass(canvas, cursorClass);
+        cursorClass = "default-cursor";
+        addClass(canvas, cursorClass);
+    } else {
+        let coords = e.lngLat;
+        app.ports.movedAnchor.send([cursorOverPoint, coords.lng, coords.lat]);
+        isDragging = false;
+        map.off("mousemove", mouseMove);
+    }
+}
+
+function mouseDown(e) {
+    if (e.originalEvent.which !== 1) return;
+    if (cursorOverPoint === null || viewOnly) {
+        removeClass(canvas, cursorClass);
+        cursorClass = "move-cursor";
+        addClass(canvas, cursorClass);
+        return;
+    }
+    isDragging = true;
+    removeClass(canvas, cursorClass);
+    cursorClass = "grabbing-cursor";
+    addClass(canvas, cursorClass);
+    map.on("mousemove", mouseMove);
+}
+
+function mouseMove(e) {
+    if (!isDragging) return;
+    removeClass(canvas, cursorClass);
+    cursorClass = "grabbing-cursor";
+    addClass(canvas, cursorClass);
+    let coords = [e.lngLat.lng, e.lngLat.lat];
+    map.getSource(cursorOverPoint).setData(makeSource("Point", coords));
+}
+
+function mouseEnter(id) {
+    return function() {
+        removeClass(canvas, cursorClass);
+        cursorClass = "grab-cursor";
+        addClass(canvas, cursorClass);
+        cursorOverPoint = id;
+        map.dragPan.disable();
+    }
+}
+
+function mouseLeave() {
+    if (isDragging) return;
+    removeClass(canvas, cursorClass);
+    cursorClass = "default-cursor";
+    addClass(canvas, cursorClass);
+    cursorOverPoint = null;
+    map.dragPan.enable();
+}
+
+// HELPER FUNCTIONS
+
 function setupMap(coords) {
     if (map) {
         return;
     }
 
-    mapboxgl.accessToken = "pk.eyJ1Ijoia2lhbWJvZ28iLCJhIjoiY2l2MWVqdWdpMDBiMDJ5bXB5aXdyY3JrdyJ9.oqpLQhZcd0yOzBKdSxyk2w";
+    mapboxgl.accessToken =
+        "pk.eyJ1Ijoia2lhbWJvZ28iLCJhIjoiY2l2MWVqdWdpMDBiMDJ5bXB5aXdyY3JrdyJ9.oqpLQhZcd0yOzBKdSxyk2w";
 
     let center, zoom;
     if (coords) {
@@ -124,267 +308,44 @@ function setupMap(coords) {
     addClass(canvas, cursorClass);
     map.dragRotate.disable();
     map.touchZoomRotate.disableRotation();
-    map.on("mousedown", onMapMouseDown);
-    map.on("mouseup", onMapMouseUp);
+    map.on("mousedown", mouseDown);
+    map.on("mouseup", mouseUp);
     map.on("zoomend", function() { app.ports.zoomLevel.send(map.getZoom()); });
 
     showingLayer = "SurfaceQuality";
+    let hiddenLayer = Object.assign({}, trafficLayer);
+    hiddenLayer.layout = { visibility: "none" };
     map.on("load", function () {
         map.addLayer(Object.assign({}, surfaceLayer));
+        map.addLayer(hiddenLayer);
     });
 }
 
-app.ports.setLayer.subscribe(function(layer) {
-    if (showingLayer === layer) {
-        return;
-    } else if (map.getLayer(layer)) {
-        map.setLayoutProperty(showingLayer, "visibility", "none");
-        map.setLayoutProperty(layer, "visibility", "visible");
-    } else if (layer === "TrafficSafety") {
-        map.setLayoutProperty(showingLayer, "visibility", "none");
-        map.addLayer(Object.assign({}, trafficLayer));
-    }
-    showingLayer = layer;
-});
-
-app.ports.refreshLayer.subscribe(function(layer) {
-    map.removeLayer(layer);
-    map.removeSource(layer);
-    if (layer === "SurfaceQuality") {
-        let dirtySurfaceLayer = Object.assign({}, surfaceLayer);
-        dirtySurfaceLayer.dirty = Math.random();
-        dirtySurfaceLayer.source.tiles[0] += "?dirty=" + Math.random();
-        map.addLayer(dirtySurfaceLayer);
-    } else {
-        let dirtyTrafficLayer = Object.assign({}, trafficLayer);
-        dirtyTrafficLayer.dirty = Math.random();
-        dirtyTrafficLayer.source.tiles[0] += "?dirty=" + Math.random();
-        map.addLayer(dirtyTrafficLayer);
-    }
-});
-
-app.ports.routeCreate.subscribe(function() {
-    viewOnly = false;
-    map.on("click", onMapClick);
-    map.on("contextmenu", onMapRightClick);
-});
-
-function onMapMouseUp(e) {
-    removeClass(canvas, cursorClass);
-    cursorClass = "default-cursor";
-    addClass(canvas, cursorClass);
-}
-
-function onMapMouseDown(e) {
-    if (e.originalEvent.which !== 1) return;
-    if (cursorOverPoint === null || viewOnly) {
-        removeClass(canvas, cursorClass);
-        cursorClass = "move-cursor";
-        addClass(canvas, cursorClass);
-        return;
-    }
-    isDragging = true;
-    removeClass(canvas, cursorClass);
-    cursorClass = "grabbing-cursor";
-    addClass(canvas, cursorClass);
-    map.on("mousemove", onMoveMarker);
-    map.once("mouseup", onDropMarker);
-}
-
-function onMoveMarker(e) {
-    if (!isDragging) return;
-    removeClass(canvas, cursorClass);
-    cursorClass = "grabbing-cursor";
-    addClass(canvas, cursorClass);
-    let coords = [e.lngLat.lng, e.lngLat.lat];
-    map.getSource(cursorOverPoint).setData(markerSource(coords));
-}
-
-function onDropMarker(e) {
-    if (!isDragging) return;
-    let coords = e.lngLat;
-    app.ports.moveAnchor.send([cursorOverPoint, coords.lng, coords.lat]);
-    isDragging = false;
-    map.off("mousemove", onMoveMarker);
-}
-
-function onMapClick(e) {
-    if (cursorOverPoint !== null || isDragging) return;
-
-    app.ports.setAnchor.send([e.lngLat.lng, e.lngLat.lat]);
-
-    // Shift LngLat for map pan to compensate for open sidebar menu
-    let mapBounds = map.getBounds();
-    let mapMinX = mapBounds.getWest();
-    let mapMaxX = mapBounds.getEast();
-    let windowMinX = 0;
-    let windowMaxX = window.innerWidth;
-    let mapPosX = e.lngLat.lng;
-    let mapPosY = e.lngLat.lat;
-    let windowPosX = windowMaxX / (mapMaxX - mapMinX) * (mapPosX - mapMinX);
-    let shiftedWindowPosX = windowPosX - 200;
-    let shiftedMapPosX = mapMinX + (mapMaxX - mapMinX) / windowMaxX * shiftedWindowPosX;
-    let shiftedLngLat = [shiftedMapPosX, mapPosY];
-
-    // Calculate pan duration based on distance to pan
-    let maxDistX = (window.innerWidth - 400) / 2;
-    let posX = Math.abs(e.originalEvent.x - 400 - maxDistX);
-    let panX = 0.25 + (0.6 / maxDistX) * (posX - 400);
-    let maxDistY = window.innerHeight / 2;
-    let posY = Math.abs(e.originalEvent.y - maxDistY);
-    let panY = 0.25 + (0.6 / maxDistY) * posY;
-    let panDuration = Math.max(panX, panY) * 1000;
-
-    map.panTo(shiftedLngLat, { duration: panDuration });
-}
-
-app.ports.addAnchor.subscribe(function(anchor) {
-    let id = anchor[0];
-    let coords = [anchor[1], anchor[2]];
-    if (map.getSource(id)) {
-        map.getSource(id).setData(markerSource(coords));
-    } else {
-        map.addSource(id, {
-            "type": "geojson",
-            "data": markerSource(coords)
-        });
-        if (id === "startMarker") {
-            map.addLayer({
-                "id": id,
-                "type": "circle",
-                "source": id,
-                "paint": {
-                    "circle-radius": 7,
-                    "circle-color": "#40B34F",
-                    "circle-stroke-color": "#FFFFFF",
-                    "circle-stroke-width": 2
-                }
-            });
-        } else {
-            map.addLayer({
-                "id": id,
-                "type": "circle",
-                "source": id,
-                "paint": {
-                    "circle-radius": 4,
-                    "circle-color": "#FFFFFF",
-                    "circle-stroke-width": 2
-                }
-            });
-        }
-    }
-
-    map.on("mouseenter", id, function() {
-        removeClass(canvas, cursorClass);
-        cursorClass = "grab-cursor";
-        addClass(canvas, cursorClass);
-        cursorOverPoint = id;
-        map.dragPan.disable();
-    });
-
-    map.on("mouseleave", id, function() {
-        if (isDragging) return;
-        removeClass(canvas, cursorClass);
-        cursorClass = "default-cursor";
-        addClass(canvas, cursorClass);
-        cursorOverPoint = null;
-        map.dragPan.enable();
-    });
-});
-
-function onMapRightClick(e) {
-    if (cursorOverPoint !== null && cursorOverPoint !== "startMarker") {
-        if (popup) {
-            popup.remove();
-        }
-        popup = new mapboxgl.Popup()
-            .setLngLat(e.lngLat)
-            .setHTML("<span style='cursor: pointer' onClick='removeMarker(\"" + cursorOverPoint + "\")'>Delete Point</span>")
-            .addTo(map);
-    }
+function makeSource(geomType, coords) {
+    return {
+        "type": "FeatureCollection",
+        "features": [{
+            "type": "Feature",
+            "geometry": {
+                "type": geomType,
+                "coordinates": coords
+            }
+        }]
+    };
 }
 
 function removeMarker(key) {
     if (popup) {
         popup.remove();
     }
-    map.getSource(key).setData(emptySource);
+    map.getSource(key).setData({
+        "type": "FeatureCollection",
+        "features": []
+    });
     app.ports.removedAnchor.send(key);
 }
 
-// DROP MAP
-app.ports.down.subscribe(function() {
-    if (map) {
-        map.remove();
-        map = null;
-    }
-});
-
-// SNAP ANCHOR
-app.ports.snapAnchor.subscribe(function(values) {
-    let pointId = values[0];
-    let coords = [values[1].lng, values[1].lat];
-    map.getSource(pointId).setData(markerSource(coords));
-});
-
-app.ports.removeAnchor.subscribe(function(pointId) {
-    removeMarker(pointId);
-});
-
-// PLOT ROUTE
-app.ports.displayRoute.subscribe(function(line) {
-    let firstPoint = line[0];
-    let id = line[1];
-    let lineCoords = [];
-    for (let i = 0; i < line[2].length; i++) {
-        lineCoords.push([line[2][i][1], line[2][i][0]]);
-    }
-    let routeSource = {
-        "type": "FeatureCollection",
-        "features": [{
-            "type": "Feature",
-            "geometry": {
-                "type": "LineString",
-                "coordinates": lineCoords
-            }
-        }]
-    };
-    if (map.getSource(id)) {
-        map.getSource(id).setData(routeSource);
-    } else {
-        map.addSource(id, {
-            "type": "geojson",
-            "data": routeSource
-        });
-        map.addLayer({
-            "id": id,
-            "type": "line",
-            "source": id,
-            "paint": {
-                "line-opacity": 0.5,
-                "line-width": 5
-            }
-        });
-    }
-    map.moveLayer(id, firstPoint);
-});
-
-app.ports.removeRoute.subscribe(function(id) {
-    if (!map.getSource(id)) return;
-    map.getSource(id).setData(emptySource);
-});
-
-// CLEAR ROUTE
-app.ports.clearRouting.subscribe(function(sources) {
-    viewOnly = true;
-    map.off("click", onMapClick);
-    map.off("contextmenu", onMapRightClick);
-    for (let i = 0; i < sources.length; i++) {
-        map.getSource(sources[i]).setData(emptySource);
-    }
-});
-
-// UTIL
+// UTIL FUNCTIONS
 
 // from http://www.openjs.com/scripts/dom/class_manipulation.php
 function hasClass(ele, cls) {
