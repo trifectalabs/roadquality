3f6265c00b53c278d99e195f1f092013f024fee4
Josiah Witt <josiah@witt.life>
2017-08-09 12:38:23 -0400

    Add timeouts to OSRM reqs + failure responses to route controller

--- api/app/controllers/MapRoutes.scala
+++ api/app/controllers/MapRoutes.scala
@@ -9,16 +9,20 @@ import play.api.libs.json.Json
 import play.api.mvc.{Action, Controller}
 import util.actions.Authenticated
 
-import scala.concurrent.ExecutionContext
+import scala.concurrent.{ExecutionContext, Future}
 
 class MapRoutes @Inject() (routingService: RoutingService)(implicit ec: ExecutionContext) extends Controller {
 
   def getByPoints(points: String) = Authenticated.async { req =>
-    routingService.generateRoute(points).map(r => Ok(Json.toJson(r)))
+    routingService.generateRoute(points)
+      .map(r => Ok(Json.toJson(r)))
+      .fallbackTo(Future(ServiceUnavailable))
   }
 
   def getSnap(lat: Double, lng: Double) = Authenticated.async { req =>
-    routingService.snapPoint(Point(lat, lng)).map(p => Ok(Json.toJson(p)))
+    routingService.snapPoint(Point(lat, lng))
+      .map(p => Ok(Json.toJson(p)))
+      .fallbackTo(Future(ServiceUnavailable))
   }
 }
 
