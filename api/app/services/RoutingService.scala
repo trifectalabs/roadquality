08de3926fb8602e07828d33b26a8c0de01ca8fd0
Josiah Witt <josiah@witt.life>
2017-08-09 12:38:23 -0400

    Add timeouts to OSRM reqs + failure responses to route controller

--- api/app/services/RoutingService.scala
+++ api/app/services/RoutingService.scala
@@ -1,12 +1,12 @@
 package services
 
 import javax.inject.Inject
-import scala.concurrent.{ExecutionContext, Future}
 
 import com.trifectalabs.roadquality.v0.models.{ Point, MapRoute }
 import com.trifectalabs.polyline.{ Polyline, LatLng }
 import db.dao.MapDao
 import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.duration._
 import play.api.libs.ws._
 import play.api.libs.json._
 import play.api.Configuration
@@ -21,7 +21,7 @@ class RoutingServiceImpl @Inject()(configuration: Configuration, ws: WSClient)(i
 
   def generateRoute(points: String): Future[MapRoute] = {
     val osrmUrl = s"$osrmUri/route/v1/cycling/$points"
-    ws.url(osrmUrl).get().map { res =>
+    ws.url(osrmUrl).withRequestTimeout(5000.millis).get().map { res =>
       val routes = (res.json \ "routes").as[List[JsValue]]
       val polyline = (routes(0) \ "geometry").as[String]
       val distance = (routes(0) \ "distance").as[Double]
@@ -36,7 +36,7 @@ class RoutingServiceImpl @Inject()(configuration: Configuration, ws: WSClient)(i
   def snapPoint(point: Point): Future[Point] = {
     val pointString = s"${point.lng},${point.lat}"
     val osrmUrl = s"$osrmUri/nearest/v1/cycling/$pointString"
-    ws.url(osrmUrl).get().map { res =>
+    ws.url(osrmUrl).withRequestTimeout(5000.millis).get().map { res =>
       val waypoints = (res.json \ "waypoints").as[List[JsValue]]
       val location = (waypoints(0) \ "location").as[List[Double]]
       Point(
